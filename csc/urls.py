12cdbeb8b65445ce217e8f23d818c7e9ab42fc06
DR <d.retnan@nhubnigeria.com>
2017-02-04 14:46:40 +0000

    using wrong settings.py

--- csc/urls.py
+++ csc/urls.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.sitemaps import CMSSitemap
-from django.conf import settings
+from . import settings
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
